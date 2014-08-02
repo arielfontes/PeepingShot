d25c41cd655b839f996b5bc302dcf9bbf36882f0
ArielFontes <arielfontes@outloock.com>
2014-08-02 19:34:37 -0300

    primeiro commit

---
+++ Assets/Script/ai_Girls.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+var wPoints:Transform[];
+var agent:NavMeshAgent;
+
+var assustada:boolean;
+
+var position:int;
+var nextPosition:int;
+
+function Start () 
+{
+	assustada=false;
+	nextPosition=0;
+	agent = GetComponent(NavMeshAgent);
+}
+function Update () 
+{
+	Movimenta();
+}
+function Movimenta()
+{
+	if(wPoints!=null)
+	{
+		agent.destination = wPoints[nextPosition].position;
+	}
+}
+function OnTriggerEnter(col:Collider)
+{
+	if(assustada==false&&col.transform.position==wPoints[nextPosition].position)
+	{
+		position = nextPosition;
+		var r:int =Random.Range(0,5);
+		yield WaitForSeconds (r);
+		nextPosition +=1;
+		if(nextPosition==wPoints.Length)
+		{
+			nextPosition=0;
+		}
+	}
+}
+
