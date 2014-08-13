84c4a4f7d45c39dec58df08246b006a7ec22c72b
ArielFontes <arielfontes@outloock.com>
2014-08-13 01:30:01 -0300

    correcao do problema de nao carregar fotos fora do editor

--- Assets/Script/AlbumFotos.cs
+++ Assets/Script/AlbumFotos.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO;
-using UnityEditor;
 using System.Collections.Generic;
 using System;
 
@@ -10,7 +9,7 @@ public class AlbumFotos : MonoBehaviour {
 	private IList<Texture2D> fotos;
 	private int listaIndex = 1;
 	public Camera cameraFPS;
-	
+
 	void Start () {
 		renderer.enabled = false;
 		carregaFotos();
@@ -23,25 +22,32 @@ public class AlbumFotos : MonoBehaviour {
 	}
 
 	private void carregaFotos(){
+
 		fotos = new List<Texture2D>();
-		string[] aNomeFotos = Directory.GetFiles("Assets/ScreenShots", "*.png");
-		foreach(string matFile in aNomeFotos){
-			string assetPath = matFile.Replace("Assets/ScreenShots/", "").Replace('\\', '/');
-			Texture2D sourceMat = (Texture2D)AssetDatabase.LoadAssetAtPath(assetPath, typeof(Texture2D));
-			fotos.Add(sourceMat);
-		}
+		string[] aNomeFotos = Directory.GetFiles(Application.dataPath + "/ScreenShots", "*.png");
 		try{
+			foreach(string matFile in aNomeFotos){ 
+				// original
+//				string assetPath = matFile.Replace("Assets/ScreenShots/", "").Replace('\\', '/');
+				// teste
+				string assetPath = matFile.Replace(Application.dataPath + "/screenshots/", "").Replace('\\', '/');
+				byte[] sourceBytes = System.IO.File.ReadAllBytes(assetPath);
+				Texture2D tex = new Texture2D(4,4);
+				tex.LoadImage(sourceBytes);
+				fotos.Add(tex);
+			}
+
 			renderer.material.mainTexture = fotos[0];
 			Debug.Log("fotos][0");
 		}catch (ArgumentOutOfRangeException){
-			Texture2D sourceMat = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/ScreenShots/noImage/noImageAvailable.png", typeof(Texture2D));
-			renderer.material.mainTexture = sourceMat;
+			renderer.material.mainTexture = (Texture2D)Resources.Load("noImageAvailable", typeof(Texture2D));
 			Debug.Log("catch");
 		}
 	}
 
 	private void exibeAlbum(){
 		if (Input.GetKeyDown(KeyCode.M)){
+			Debug.Log("MMM");
 			carregaFotos();
 			cameraFPS.enabled = !cameraFPS.enabled;
 			renderer.enabled = !renderer.enabled;
--- Assets/Script/CameraFotoControle.cs
+++ Assets/Script/CameraFotoControle.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System.Collections;
-using UnityEditor;
 
 public class CameraFotoControle : MonoBehaviour {
 	public Camera cameraFoto;
 	private bool takeShot = false;
+
+	// teste
+	public int resWidth = 840;
+	public int resHeight = 620;
 		
 	void Update() {
 		tiraFoto();
@@ -22,8 +25,8 @@ public class CameraFotoControle : MonoBehaviour {
 		cameraFoto.enabled = takeShot;
 		
 		if (takeShot && Input.GetMouseButtonDown(0)) {
-			Application.CaptureScreenshot(ScreenShotName());
-			AssetDatabase.Refresh();
+//			Application.CaptureScreenshot(ScreenShotName());
+			tiraFoto2();
 			Debug.Log(string.Format("Took screenshot"));
 		}
 	}
@@ -34,6 +37,34 @@ public class CameraFotoControle : MonoBehaviour {
 	/// <returns>The shot name.</returns>
 	public static string ScreenShotName() {
 		return string.Format("{0}/screenshots/screen_{1}.png", Application.dataPath, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+	}
+
+	// teste outro screenshot
+	void tiraFoto2() {
+		takeShot |= Input.GetKeyDown("k");
+		if (takeShot) {
+			RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+			cameraFoto.targetTexture = rt;
+			Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+			cameraFoto.Render();
+			RenderTexture.active = rt;
+			screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+			cameraFoto.targetTexture = null;
+			RenderTexture.active = null; // JC: added to avoid errors
+			Destroy(rt);
+			byte[] bytes = screenShot.EncodeToPNG();
+			string filename = ScreenShotName2(resWidth, resHeight);
+			System.IO.File.WriteAllBytes(filename, bytes);
+			Debug.Log(string.Format("Took screenshot to: {0}", filename));
+			takeShot = false;
+		}
+	}
+
+	public static string ScreenShotName2(int width, int height) {
+		return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+		                     Application.dataPath,
+		                     width, height,
+		                     System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
 	}
 	
 }
